400e1516d23826ac8d3a36c39620f6fd3a7d532c
macharia <mrmachariaorsam@gmail.com>
2025-05-09 22:32:51 +0300

    last commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+   num_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+   pass
+
 
 def make_exclamation(sentence_list):
+    sentence_list(["Hello!","I,m doing great!","Python is fun!"])
     pass
